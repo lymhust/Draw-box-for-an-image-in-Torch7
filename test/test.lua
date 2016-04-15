09fa847420c3849eb7a2468767238ede8a2d62a1
Yuming LI <lymhust@gmail.com>
2016-04-15 17:46:56 +0800

    Added files via upload

--- test/test.lua
+++ test/test.lua
@@ -1,3 +1,4 @@
 draw = require 'draw'
 require 'image'
 image.display(draw.drawBox(image.lena(), 0.5, 0.5, 0.9, 0.9, 5, {0.3,1,0.7}))
+image.display(draw.drawBox(image.rgb2y(image.lena()), 0.5, 0.5, 0.9, 0.9, 5))
